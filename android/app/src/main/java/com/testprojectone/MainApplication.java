dc2bbccaa9c139ce1e35bc72caea995215cca90a
Albert-cord <fjr_studio@163.com>
2022-08-13 16:35:26 +0800

    fix: rollback origin flipper

--- android/app/src/main/java/com/testprojectone/MainApplication.java
+++ android/app/src/main/java/com/testprojectone/MainApplication.java
@@ -79,7 +79,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.codepushdemoapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.testprojectone.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
